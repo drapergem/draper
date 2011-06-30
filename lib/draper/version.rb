d053510bb6ba80e549ecdb170baf063f3ea3f9db
Jeff Casimir <jeff@casimircreative.com>
2011-06-30 18:46:09 -0400

    update generated model's instructions to use allows/denies

--- lib/draper/version.rb
+++ lib/draper/version.rb
@@ -1,3 +1,3 @@
 module Draper
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 end
--- lib/generators/draper/model/templates/model.rb
+++ lib/generators/draper/model/templates/model.rb
@@ -1,13 +1,25 @@
 class <%= singular_name.camelize %>Decorator < Draper::Base
-  # Rails helpers like content_tag, link_to, and pluralize are already
-  # available to you. If you need access to other helpers, include them
-  # like this:
-  #   include ActionView::Helpers::TextHelper
-  # Or pull in the whole kitchen sink:
-  #   include ActionView::Helpers
 
-  # Then define presentation-related instance methods. Ex:
-  #   def formatted_created_at
-  #     content_tag :span, created_at.strftime("%A")
-  #   end
+  # Rails Helpers
+  #   Rails helpers like content_tag, link_to, and pluralize are already
+  #   available to you. If you need access to other helpers, include them
+  #   like this:
+  #     include ActionView::Helpers::TextHelper
+  #   Or pull in the whole kitchen sink:
+  #     include ActionView::Helpers
+
+  # Wrapper Methods
+  #   Control access to the wrapped subject's methods using one of the following:
+  #
+  #   To allow _only_ the listed methods:
+  #     allows :method1, :method2
+  #
+  #   To allow everything _except_ the listed methods:
+  #     denies :method1, :method2
+
+  # Presentation Methods
+  #   Define presentation-related instance methods. Ex:
+  #     def formatted_created_at
+  #       content_tag :span, created_at.strftime("%A")
+  #     end
 end
