d7f154c97887840288bd56ee573ff1ae1b1e76a0
Jeff Casimir <jeff@casimircreative.com>
2011-07-29 20:22:18 -0500

    Generate and inherit from ApplicationDecorator

--- lib/generators/draper/model/model_generator.rb
+++ lib/generators/draper/model/model_generator.rb
@@ -2,8 +2,9 @@ module Draper
   class ModelGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
-    def build_model
+    def build_model_and_application_decorators
       empty_directory "app/decorators"
+      template 'application_decorator.rb', 'app/decorators/application_decorator.rb'
       template 'model.rb', "app/decorators/#{singular_name}_decorator.rb"
     end
   end
