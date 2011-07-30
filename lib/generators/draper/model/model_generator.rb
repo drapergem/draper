09416e77e1c262937e9112b13d425e62a2e453b9
Jeff Casimir <jeff@casimircreative.com>
2011-07-30 09:33:39 +0800

    Don't attempt to overwrite an existing ApplicationDecorator

--- lib/generators/draper/model/model_generator.rb
+++ lib/generators/draper/model/model_generator.rb
@@ -2,10 +2,16 @@ module Draper
   class ModelGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
+    DECORATORS_ROOT = 'app/decorators/'
+    APPLICATION_DECORATOR = 'application_decorator.rb'
+    APPLICATION_DECORATOR_PATH = DECORATORS_ROOT + APPLICATION_DECORATOR
+
     def build_model_and_application_decorators
       empty_directory "app/decorators"
-      template 'application_decorator.rb', 'app/decorators/application_decorator.rb'
-      template 'model.rb', "app/decorators/#{singular_name}_decorator.rb"
+      unless File.exists?(APPLICATION_DECORATOR_PATH)
+        template APPLICATION_DECORATOR, APPLICATION_DECORATOR_PATH
+      end
+      template 'model.rb', "#{DECORATORS_ROOT}#{singular_name}_decorator.rb"
     end
   end
 end
