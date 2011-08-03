04fe62db0fca5eeef4af91ecf942f7e73f3b7755
Jeff Casimir <jeff@casimircreative.com>
2011-08-02 23:03:17 -0700

    Rename existing generator from draper:model to draper:decorator

--- lib/draper.rb
+++ lib/draper.rb
@@ -1,7 +1,8 @@
-require "draper/version"
+require 'draper/version'
 require 'draper/system'
 require 'draper/all_helpers'
 require 'draper/base'
 require 'draper/lazy_helpers'
+require 'draper/railtie'
 
 Draper::System.setup
