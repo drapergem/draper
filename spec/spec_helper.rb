3edcab921a155e7f7e4c72a30d7d777171fbf67b
Jeff Casimir <jeff@casimircreative.com>
2011-08-27 23:52:03 -0500

    Simplifying loading in spec_helper thanks to new dependency style

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,8 @@
 require 'rubygems'
-require 'bundler'
-
-require 'rspec'
-begin
-  require 'cover_me'
-rescue LoadError
-  # Silently fail
-end
-require './spec/samples/application_helper.rb'
-Bundler.require
-Dir.glob('./spec/samples/*') {|file| require file}
-require 'active_support'
-require 'action_view'
 require 'bundler/setup'
-
+Bundler.require
 require 'draper'
-
-Dir["spec/support/**/*.rb"].each do |file|
-  require "./" + file
-end
-
-RSpec.configure do |config|
-
+require './spec/samples/application_helper.rb'
+Dir.glob(['./spec/samples/*.rb', './spec/support/*.rb']) do |file| 
+  require file
 end
