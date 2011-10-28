c56ac5ed2c45e1a1a955667d4fcc3646baf8bb3b
Benny Wong <benny@bwong.net>
2011-10-28 13:16:25 -0400

    Include Draper::ViewContextFilter in ActionMailer::Base
    * Only send the :before_filter method if it exists
    * This will allow decorators to be used when rendering email

--- lib/draper/view_context.rb
+++ lib/draper/view_context.rb
@@ -8,14 +8,14 @@ module Draper
       Thread.current[:current_view_context] = input
     end
   end
-  
+
   module ViewContextFilter
     def set_current_view_context
       Draper::ViewContext.current = self.view_context
     end
-    
+
     def self.included(source)
-      source.send(:before_filter, :set_current_view_context)
+      source.send(:before_filter, :set_current_view_context) if source.respond_to?(:before_filter)
     end
   end
 end
