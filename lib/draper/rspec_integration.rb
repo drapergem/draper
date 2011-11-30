5e58b82ca10417a814775e355fe1b660fb5ba249
Jeff Casimir <jeff@casimircreative.com>
2011-11-30 11:31:23 -0500

    Using 1.8-compatible hash syntax

--- lib/draper/rspec_integration.rb
+++ lib/draper/rspec_integration.rb
@@ -6,8 +6,8 @@ module Draper
 
   RSpec.configure do |config|
     # Automatically tag specs in specs/decorators as type: :decorator
-    config.include Draper::DecoratorExampleGroup, type: :decorator, example_group: {
-      file_path: /spec[\\\/]decorators/
+    config.include Draper::DecoratorExampleGroup, :type => :decorator, :example_group => {
+      :file_path => /spec[\\\/]decorators/
     }
 
     # Specs tagged type: :decorator set the Draper view context
