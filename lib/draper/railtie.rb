3e4eaf14ad0accfd9db61224a8e80a96437da89f
Alexandre Angelim <angelim@angelim.com.br>
2011-12-19 21:09:15 -0200

    Railtie must reload decorators at every request

--- lib/draper/railtie.rb
+++ lib/draper/railtie.rb
@@ -1,8 +1,8 @@
 module Draper
   # Requiring decorators so they can be registered by models on initialization
   class Railtie < Rails::Railtie
-    initializer "Require decorators" do |app|
-      config.after_initialize do
+    initializer "draper.load_dependencies" do |app|
+      app.config.to_prepare do
         Dir.glob("#{Rails.root}/app/decorators/**/*.rb").each{ |file| require_dependency file}
       end
     end
