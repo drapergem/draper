10d2a6ed9acc4658224801a2414e4dd283d55343
Steve Klabnik <steve@steveklabnik.com>
2012-01-05 10:47:18 -0500

    explicitly require-ing to fix odd order issue

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,15 @@ require 'bundler/setup'
 Bundler.require
 
 require './spec/support/samples/active_record.rb'
-Dir['./spec/support/**/*.rb'].each {|file| require file }
+require './spec/support/samples/application_controller.rb'
+require './spec/support/samples/application_helper.rb'
+require './spec/support/samples/decorator.rb'
+require './spec/support/samples/decorator_with_allows.rb'
+require './spec/support/samples/decorator_with_application_helper.rb'
+require './spec/support/samples/decorator_with_denies.rb'
+require './spec/support/samples/namespaced_product.rb'
+require './spec/support/samples/namespaced_product_decorator.rb'
+require './spec/support/samples/product.rb'
+require './spec/support/samples/product_decorator.rb'
+require './spec/support/samples/widget.rb'
+require './spec/support/samples/widget_decorator.rb'
