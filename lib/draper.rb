ef368a3a88f951661e9d7cb322c8ada88778e98b
Steve Klabnik <steve@steveklabnik.com>
2012-01-05 18:22:42 -0500

    Only include rspec if we really want it.
    This closes #91.

--- lib/draper.rb
+++ lib/draper.rb
@@ -6,6 +6,6 @@ require 'draper/model_support'
 require 'draper/helper_support'
 require 'draper/view_context'
 require 'draper/decorated_enumerable_proxy'
-require 'draper/rspec_integration' if defined?(RSpec)
+require 'draper/rspec_integration' if defined?(RSpec) and RSpec.respond_to?(:configure)
 
 Draper::System.setup
