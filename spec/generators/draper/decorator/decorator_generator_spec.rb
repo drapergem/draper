7e45a1f7723eb40ed63d379396e4e27b09ef091c
Thiago Almeida <tapgyn@gmail.com>
2012-02-14 00:19:03 -0200

    draper:decorator defaults to rspec too

--- spec/generators/draper/decorator/decorator_generator_spec.rb
+++ spec/generators/draper/decorator/decorator_generator_spec.rb
@@ -9,6 +9,50 @@ describe Draper::DecoratorGenerator do
 
   before { prepare_destination }
 
+  context 'decorator context' do
+    before { run_generator ["product"]  }
+
+    describe 'app/decorators/product_decorator.rb' do
+      subject { file('app/decorators/product_decorator.rb') }
+      it { should exist }
+      it { should contain "class ProductDecorator < ApplicationDecorator" }
+    end
+  end
+
+  context 'default test framework' do
+    before { run_generator ["product"]  }
+
+    describe 'spec/decorators/product_decorator_spec.rb' do
+      subject { file('spec/decorators/product_decorator_spec.rb') }
+      it { should exist }
+      it { should contain "describe ProductDecorator" }
+    end
+  end
+
+  context 'using rspec' do
+    before { run_generator ["product", "-t=rspec"]  }
+
+    describe 'spec/decorators/product_decorator_spec.rb' do
+      subject { file('spec/decorators/product_decorator_spec.rb') }
+      it { should exist }
+      it { should contain "describe ProductDecorator" }
+    end
+  end
+
+  context 'using rspec' do
+    before { run_generator ["product", "-t=test_unit"]  }
+
+    describe 'test/decorators/product_decorator_test.rb' do
+      subject { file('test/decorators/product_decorator_test.rb') }
+      it { should exist }
+      it { should contain "class ProductDecoratorTest < ActiveSupport::TestCase" }
+    end
+  end
+  
+end
+
+
+=begin
   describe 'no arguments' do
     before { run_generator %w(products)  }
 
@@ -17,6 +61,43 @@ describe Draper::DecoratorGenerator do
       it { should exist }
       it { should contain "class ProductsDecorator < ApplicationDecorator" }
     end
-
   end
-end
+
+  
+  context 'simple' do
+    before { run_generator %w(products)  }
+
+    describe 'app/decorators/products_decorator.rb' do
+      subject { file('app/decorators/products_decorator.rb') }
+      it { should exist }
+      it { should contain "class ProductsDecorator < ApplicationDecorator" }
+    end
+  end
+
+
+
+
+
+  context 'using rspec' do
+
+    describe 'app/decorators/products_decorator.rb' do
+      subject { file('app/decorators/products_decorator.rb') }
+      it { should exist }
+      it { should contain "class ProductsDecorator < ApplicationDecorator" }
+    end
+
+    shared_examples_for "ApplicationDecoratorGenerator" do
+      describe 'app/decorators/application_decorator.rb' do
+        subject { file('app/decorators/application_decorator.rb') }
+        it { should exist }
+        it { should contain "class ApplicationDecorator < Draper::Base" }
+      end
+    end
+
+    describe 'spec/decorators/application_decorator_spec.rb' do
+      subject { file('spec/decorators/application_decorator_spec.rb') }
+      it { should exist }
+      it { should contain "describe ApplicationDecorator do" }
+    end
+  end
+=end
