8f37b95f47b719c9d47b8ef6ac79863858e52586
Mark Burns <markthedeveloper@gmail.com>
2012-03-04 05:22:10 +0000

    removed superfluous begin block

--- lib/draper/lazy_helpers.rb
+++ lib/draper/lazy_helpers.rb
@@ -1,11 +1,9 @@
 module Draper
   module LazyHelpers
     def method_missing(method_name, *args, &block)
-      begin
-        helpers.send method_name, *args, &block
-      rescue NoMethodError
-        super
-      end
+      helpers.send method_name, *args, &block
+    rescue NoMethodError
+      super
     end
   end
 end
