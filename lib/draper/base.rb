b3c149ce068538eb9f927fc22bb332b11c75dd22
Mark Burns <markthedeveloper@googlemail.com>
2012-03-15 08:08:55 +0000

    I just noticed we had duplicate logic in the check within method missing. We were checking if the method was allowed and if the model responds to the method. However this is the same check within respond_to?. Removing this check doesn't introduce any test failures so it looks like this is the right thing to do

--- lib/draper/base.rb
+++ lib/draper/base.rb
@@ -208,8 +208,7 @@ module Draper
     end
 
     def method_missing(method, *args, &block)
-      super unless allow?(method)
-      super unless model.respond_to? method
+      super unless respond_to? method
 
       self.class.send :define_method, method do |*args, &block|
         model.send method, *args, &block
