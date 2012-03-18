4293d307c31ea96e62eb2f055c932c3d494eed4a
Vasiliy Ermolovich <younash@gmail.com>
2012-03-19 00:01:52 +0300

    use let instead of before hook

--- spec/draper/helper_support_spec.rb
+++ spec/draper/helper_support_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe Draper::HelperSupport do
-  before(:each){ @product = Product.new}
+  let(:product){ Product.new }
 
   context '#decorate' do
     it 'renders a block' do
-      output = ApplicationController.decorate(@product){|p| p.model.object_id }
-      output.should == @product.object_id
+      output = ApplicationController.decorate(product){|p| p.model.object_id }
+      output.should == product.object_id
     end
 
     it 'uses #capture so Rails only renders the content once' do
-      ApplicationController.decorate(@product){|p| p.model.object_id }
+      ApplicationController.decorate(product){|p| p.model.object_id }
       ApplicationController.capture_triggered.should be
     end
   end
