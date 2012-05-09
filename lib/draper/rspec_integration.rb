91680b12640fa15c88ac4b5cfdce3f9700b9c457
Steve Klabnik <steve@steveklabnik.com>
2012-05-09 14:29:32 -0400

    Adding capybara integration.
    Fixes #156

--- lib/draper/rspec_integration.rb
+++ lib/draper/rspec_integration.rb
@@ -3,22 +3,31 @@ module Draper
     extend ActiveSupport::Concern
     included { metadata[:type] = :decorator }
   end
+end
 
-  RSpec.configure do |config|
-    # Automatically tag specs in specs/decorators as type: :decorator
-    config.include Draper::DecoratorExampleGroup, :type => :decorator, :example_group => {
-      :file_path => /spec[\\\/]decorators/
-    }
+RSpec.configure do |config|
+  # Automatically tag specs in specs/decorators as type: :decorator
+  config.include Draper::DecoratorExampleGroup, :type => :decorator, :example_group => {
+    :file_path => /spec[\\\/]decorators/
+  }
 
-    # Specs tagged type: :decorator set the Draper view context
-    config.around do |example|
-      if :decorator == example.metadata[:type]
-        ApplicationController.new.set_current_view_context
-        Draper::ViewContext.current.controller.request ||= ActionController::TestRequest.new
-        Draper::ViewContext.current.request            ||= Draper::ViewContext.current.controller.request
-        Draper::ViewContext.current.params             ||= {}
-      end
-      example.call
+  # Specs tagged type: :decorator set the Draper view context
+  config.around do |example|
+    if :decorator == example.metadata[:type]
+      ApplicationController.new.set_current_view_context
+      Draper::ViewContext.current.controller.request ||= ActionController::TestRequest.new
+      Draper::ViewContext.current.request            ||= Draper::ViewContext.current.controller.request
+      Draper::ViewContext.current.params             ||= {}
     end
+    example.call
   end
 end
+
+if defined?(Capybara)
+  require 'capybara/rspec/matchers'
+
+  RSpec.configure do |config|
+    config.include Capybara::RSpecMatchers, type: :decorator
+  end
+end
+
