c418ec8a54526547f6b876a14eea6585de191096
Indrek Juhkam <indrek@urgas.eu>
2012-08-24 14:39:55 +0300

    Decorate struct objects correctly
    Struct objects implement #each, but they should not be decorated using
    DecoratedEnumerableProxy.

--- lib/draper/base.rb
+++ lib/draper/base.rb
@@ -148,7 +148,7 @@ module Draper
       if input.instance_of?(self)
         input.options = options unless options.empty?
         return input
-      elsif input.respond_to?(:each) && (!defined?(Sequel) || !input.is_a?(Sequel::Model))
+      elsif input.respond_to?(:each) && !input.is_a?(Struct) && (!defined?(Sequel) || !input.is_a?(Sequel::Model))
         Draper::DecoratedEnumerableProxy.new(input, self, options)
       elsif options[:infer]
         input.decorator(options)
