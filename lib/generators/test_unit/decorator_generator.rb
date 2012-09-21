6f0d5aae9fca2806311860284e755995adec500f
Vasiliy Ermolovich <younash@gmail.com>
2012-09-21 19:02:06 +0300

    handle namespaced models in spec generators
    closes #279

--- lib/generators/test_unit/decorator_generator.rb
+++ lib/generators/test_unit/decorator_generator.rb
@@ -3,7 +3,7 @@ module TestUnit
     source_root File.expand_path('../templates', __FILE__)
 
     def create_test_file
-      template 'decorator_test.rb', File.join('test/decorators', "#{singular_name}_decorator_test.rb")
+      template 'decorator_test.rb', File.join('test/decorators', class_path, "#{singular_name}_decorator_test.rb")
     end
   end
 end
