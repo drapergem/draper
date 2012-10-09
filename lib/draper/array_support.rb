1461eae62ccd0c5eb708af43098f5c34fc1c75d5
Sean Devine <barelyknown@icloud.com>
2012-10-08 21:26:21 -0500

    Add return of array member if doesnt respond

--- lib/draper/array_support.rb
+++ lib/draper/array_support.rb
@@ -1,7 +1,7 @@
 module Draper::ArraySupport
   
   def decorate
-    collect { |item| item.respond_to? :decorator_class ? item.decorate : item }
+    collect { |item| item.respond_to?(:decorator_class) ? item.decorate : item }
   end
   
 end
