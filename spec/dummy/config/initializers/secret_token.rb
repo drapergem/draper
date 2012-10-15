e82d2dc277277469e9ed98c8b2afa2fc7d36cf35
Steve Klabnik <steve@steveklabnik.com>
2012-10-15 11:32:55 -0700

    Adding cucumber stuff for integration testing.
    I've embedded a little Rails app so that we can write cuke tests and
    make sure that stuff doesn't break. Woo!
    
    This was generated by making a rails engine, then copying just the
    app over.
    
    Thanks @nashby for fixing my stupid bug with sqlite3; it had to be
    added as a development dependency. :heart:

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c2e3474d3816f60bf6dd0f3b983e7283c7ff5373e11a96935340b544a31964dbe5ee077136165ee2975e0005f5e80207c0059e6d5589699031242ba5a06dcb87'
