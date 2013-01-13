97ee7d05549f76b4b193433d1830063524531743
Andrew Haines <andrew@haines.org.nz>
2013-01-13 10:54:38 +0000

    Remove pending spec on Rails 3.0

--- spec/dummy/spec/models/post_spec.rb
+++ spec/dummy/spec/models/post_spec.rb
@@ -6,7 +6,6 @@ describe Post do
     subject { Post.first }
 
     it "is true for other instances' decorators" do
-      pending if Rails.version.start_with?("3.0")
       other = Post.first
       subject.should_not be other
       (subject == other.decorate).should be_true
