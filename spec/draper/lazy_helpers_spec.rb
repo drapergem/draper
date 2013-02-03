83794af7ea67ac6340d5d4870a607352baaa7199
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-02-03 20:34:13 +0100

    added missing spec for LazyHelpers

---
+++ spec/draper/lazy_helpers_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+module Draper
+  describe LazyHelpers do
+    describe "#method_missing" do
+      let(:decorator) do
+        result = Class.new{include Draper::LazyHelpers}.new
+        result.stub :helpers, double
+        result
+      end
+
+      it "proxies methods to #helpers" do
+        decorator.helpers.stub(:foo).and_return{|arg| arg}
+        expect(decorator.foo(:passed)).to be :passed
+      end
+
+      it "passes blocks" do
+        decorator.helpers.stub(:foo).and_return{|&block| block.call}
+        expect(decorator.foo{:yielded}).to be :yielded
+      end
+    end
+  end
+end
