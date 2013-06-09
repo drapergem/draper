5344706d54f085b7b7483f4e12bc1e1015c2de3a
Azul <azul@riseup.net>
2013-06-09 09:07:26 +0200

    remove check for parent_class_name from decorator template
    The only way I see to trigger the else case in this clause is to explicitly set the --parent option to something blank. I do not see why you would want to do this but I might be missing something here.
    
    If this is on purpose it lacks a spec and the naming strategy is inconsistent in that it does not add the Decorator to the class_name.

--- lib/generators/rails/templates/decorator.rb
+++ lib/generators/rails/templates/decorator.rb
@@ -1,9 +1,5 @@
 <%- module_namespacing do -%>
-  <%- if parent_class_name.present? -%>
 class <%= class_name %>Decorator < <%= parent_class_name %>
-  <%- else -%>
-class <%= class_name %>
-  <%- end -%>
   delegate_all
 
   # Define presentation-specific methods here. Helpers are accessed through
