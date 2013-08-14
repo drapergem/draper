2ff2213d0e0c2f87cb8cbb14c74a3ba9b3621d99
Linus Pettersson <linus.pettersson@gmail.com>
2013-08-14 12:06:49 +0200

    Renamed minitest helper to test_helper

--- lib/generators/mini_test/templates/decorator_spec.rb
+++ lib/generators/mini_test/templates/decorator_spec.rb
@@ -1,4 +1,4 @@
-require 'minitest_helper'
+require 'test_helper'
 
 describe <%= class_name %>Decorator do
 end
--- lib/generators/mini_test/templates/decorator_test.rb
+++ lib/generators/mini_test/templates/decorator_test.rb
@@ -1,4 +1,4 @@
-require 'minitest_helper'
+require 'test_helper'
 
 class <%= class_name %>DecoratorTest < Draper::TestCase
 end
