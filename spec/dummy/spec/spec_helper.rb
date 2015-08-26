ed5c3c6c8adf958d06e23f56861f4210d13f6ac8
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-26 15:35:03 -0600

    Refactored spec setup and helpers
    Moved dummy/spec to spec, symlinked dummy/spec to spec.
    This should make the test suite easier to maintain, especially given
    the changes to RSpec v3. Should also mitigate NameErrors and LoadErrors
    when running the specs.

--- spec/dummy/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
-
-RSpec.configure do |config|
-  config.expect_with(:rspec) {|c| c.syntax = :expect}
-  config.order = :random
-end
