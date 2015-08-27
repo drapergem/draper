89ca9307eeee740a27228bb83eb6140ce0c22f7e
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-26 21:43:50 -0600

    Update to RSpec 3.3
    Update gemspec to depend on rspec ~>3.3. Generate standard rails_helper
    and spec_helper. Make trivial configuration edits to both spec_helpers.
    
    Update spec_helper to disable monkey patching, and explicitly use new
    RSpec 3.0 syntax
    
    ActiveRecord::Migration.maintain_test_schema! is undefined in Rails
    4.0, so I made that method conditional upon ENV['RAILS_VERSION']

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,27 @@ require 'action_controller'
 require 'action_controller/test_case'
 
 RSpec.configure do |config|
-  config.expect_with(:rspec) {|c| c.syntax = :expect}
-  config.order = :random
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
   config.mock_with :rspec do |mocks|
+    # mocks.verify_partial_doubles = true
     mocks.yield_receiver_to_any_instance_implementation_blocks = true
   end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  else
+    config.default_formatter = 'progress'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
 end
 
 class Model; include Draper::Decoratable; end
