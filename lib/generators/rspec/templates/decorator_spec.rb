1ce287d3673da69ab81733fe0029eb6edb49b9e7
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-26 20:28:24 -0600

    Update RSpec decorator spec generator to use RSpec::Core::Version, because RSpec::Rails::Version doesn't exist.

--- lib/generators/rspec/templates/decorator_spec.rb
+++ lib/generators/rspec/templates/decorator_spec.rb
@@ -1,9 +1,9 @@
-<% if RSpec::Rails::Version::STRING.match(/\A3\.[^10]/) %>
+<% if RSpec::Core::Version::STRING.match(/\A3\.[^10]/) %>
   require 'rails_helper'
 <% else %>
   require 'spec_helper'
 <% end %>
 
 RSpec.describe <%= class_name %>Decorator, type: :decorator do
-  
+
 end
