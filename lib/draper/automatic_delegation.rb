74ad21376d4f43f99ab5d20e0e85e4c1988bc28e
Artem Bolshakov <abolshakov@spbtv.com>
2016-01-29 19:08:00 +0300

    Allow to call super inside decorator
    ```ruby
    class Post
      def title
        "calling super from A to Z"
      end
    end
    
    class PostDecorator < Draper::Decorator
      delegate_all
    
      def title
        super.upcase
      end
    end
    
    post = PostDecorator.new(Post.new)
    post.title #=> "CALLING SUPER FROM A TO Z"
    post.title #=> "calling super from A to Z"
    ```
    
    This commit fixes the issue, so you can safely call super inside overriden method.

--- lib/draper/automatic_delegation.rb
+++ lib/draper/automatic_delegation.rb
@@ -6,8 +6,12 @@ module Draper
     def method_missing(method, *args, &block)
       return super unless delegatable?(method)
 
-      self.class.delegate method
-      send(method, *args, &block)
+      if self.class.method_defined?(method)
+        object.send(method, *args, &block)
+      else
+        self.class.delegate(method)
+        send(method, *args, &block)
+      end
     end
 
     # Checks if the decorator responds to an instance method, or is able to
