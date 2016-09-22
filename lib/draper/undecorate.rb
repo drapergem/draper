c03370c9bead0d7f1ba67888f050985a9f7abb95
jarosluv <jarosluv@users.noreply.github.com>
2016-09-22 18:13:04 +0300

    Add undecorate_chain feature

--- lib/draper/undecorate.rb
+++ lib/draper/undecorate.rb
@@ -6,4 +6,12 @@ module Draper
       object
     end
   end
+  
+  def self.undecorate_chain(object)
+    if object.respond_to?(:decorated?) && object.decorated?
+      undecorate_chain(object.object)
+    else
+      object
+    end
+  end
 end
