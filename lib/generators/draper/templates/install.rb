11589fd4ea76075fedd3e70c9e71e8abffd208ac
C.J. Howard <cj@interactiveshift.com>
2017-04-03 19:46:57 -0500

    remove module namespacing

--- lib/generators/draper/templates/install.rb
+++ lib/generators/draper/templates/install.rb
@@ -1,13 +1,11 @@
-<%- module_namespacing do -%>
-  class ApplicationDecorator < Draper::Decorator
-    delegate_all
+class ApplicationDecorator < Draper::Decorator
+  delegate_all
 
-    # Set methods for all decorated objects.
-    # Helpers are accessed through `helpers` (aka `h`). For example:
-    #
-    #   def percent_amount
-    #     h.number_to_percentage object.amount, precision: 2
-    #   end
+  # Set methods for all decorated objects.
+  # Helpers are accessed through `helpers` (aka `h`). For example:
+  #
+  #   def percent_amount
+  #     h.number_to_percentage object.amount, precision: 2
+  #   end
 
-  end
-<% end -%>
+end
