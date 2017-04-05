511993f16b77aae0c06da7d2ea315f7ff5e5ce67
C.J. Howard <cj@interactiveshift.com>
2017-04-05 11:50:18 -0500

    remove delegate_all from application_decorator template

--- lib/generators/draper/templates/application_decorator.rb
+++ lib/generators/draper/templates/application_decorator.rb
@@ -1,11 +1,8 @@
 class ApplicationDecorator < Draper::Decorator
-  delegate_all
-
   # Define methods for all decorated objects.
   # Helpers are accessed through `helpers` (aka `h`). For example:
   #
   #   def percent_amount
   #     h.number_to_percentage object.amount, precision: 2
   #   end
-
 end
