7829c464fcff432a3c9857b232dfb6903851e540
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-05-08 13:28:46 +0300

    update rocket hash syntax in schema from dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019115657) do
+ActiveRecord::Schema.define(version: 20121019115657) do
 
-  create_table "posts", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "posts", force: true do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
