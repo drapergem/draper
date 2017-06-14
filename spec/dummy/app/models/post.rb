ef5f62939c422f1ebd6a907b35b1d5ae0f4064ad
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-06-14 13:42:23 +0300

    models in rails 5.* should be inherited from abstract application record class

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -1,3 +1,3 @@
-class Post < ActiveRecord::Base
+class Post < ApplicationRecord
   # attr_accessible :title, :body
 end
--- spec/dummy/spec/models/post_spec.rb
+++ spec/dummy/spec/models/post_spec.rb
@@ -3,4 +3,6 @@ require 'shared_examples/decoratable'
 
 describe Post do
   it_behaves_like "a decoratable model"
+
+  it { should be_a ApplicationRecord }
 end
