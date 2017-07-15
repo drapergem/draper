f4a4a7e0035b9eecf26506734702b8040b048ca7
Yoshiyuki Hirano <yhirano@me.com>
2017-07-15 22:53:27 +0900

    Refactor generator for TestUnit

--- lib/generators/test_unit/decorator_generator.rb
+++ lib/generators/test_unit/decorator_generator.rb
@@ -1,9 +1,12 @@
 module TestUnit
-  class DecoratorGenerator < ::Rails::Generators::NamedBase
-    source_root File.expand_path('../templates', __FILE__)
+  module Generators
+    class DecoratorGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
+      check_class_collision suffix: "DecoratorTest"
 
-    def create_test_file
-      template 'decorator_test.rb', File.join('test/decorators', class_path, "#{singular_name}_decorator_test.rb")
+      def create_test_file
+        template 'decorator_test.rb', File.join('test/decorators', class_path, "#{singular_name}_decorator_test.rb")
+      end
     end
   end
 end
