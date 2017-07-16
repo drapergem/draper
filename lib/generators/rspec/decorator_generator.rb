99d5698663e1248bee7612d0ec11ebd141479368
Yoshiyuki Hirano <yhirano@me.com>
2017-07-17 05:20:10 +0900

    Refactor source_root for cosme

--- lib/generators/rspec/decorator_generator.rb
+++ lib/generators/rspec/decorator_generator.rb
@@ -1,7 +1,7 @@
 module Rspec
   module Generators
     class DecoratorGenerator < ::Rails::Generators::NamedBase
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path("templates", __dir__)
 
       def create_spec_file
         template 'decorator_spec.rb', File.join('spec/decorators', class_path, "#{singular_name}_decorator_spec.rb")
--- lib/generators/test_unit/decorator_generator.rb
+++ lib/generators/test_unit/decorator_generator.rb
@@ -1,7 +1,7 @@
 module TestUnit
   module Generators
     class DecoratorGenerator < ::Rails::Generators::NamedBase
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path("templates", __dir__)
       check_class_collision suffix: "DecoratorTest"
 
       def create_test_file
