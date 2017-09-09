28b61d87862d8aa38ee86b45310b5b282b9743a5
Cliff Braton <cliff.braton@gmail.com>
2017-09-08 21:47:43 -0500

    Add test for #to_global_id.

--- spec/dummy/app/jobs/publish_post_job.rb
+++ spec/dummy/app/jobs/publish_post_job.rb
@@ -2,6 +2,6 @@ class PublishPostJob < ActiveJob::Base
   queue_as :default
 
   def perform(post)
-    Rails.logger.debug "Publishing post: #{post.id} of type #{post.class}"
+    post.save!
   end
 end
--- spec/dummy/spec/jobs/publish_post_job_spec.rb
+++ spec/dummy/spec/jobs/publish_post_job_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe PublishPostJob, type: :job do
   let(:post) { Post.create.decorate }
 
   subject(:job) { described_class.perform_later(post) }
-  
+
   it 'queues the job' do
-  	expect { job }.to have_enqueued_job(described_class).with(post.object)
+    expect { job }.to have_enqueued_job(described_class).with(post.object)
   end
 end
