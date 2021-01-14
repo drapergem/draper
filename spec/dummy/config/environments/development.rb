4edc462b61589138f7b67aa6dbc5309746559e7d
Tobias Bühlmann <tbuehlmann@users.noreply.github.com>
2021-01-14 19:59:46 +0100

    Fix decoration spec (#895)
    Co-authored-by: Tobias Bühlmann <buehlmann@creative-network.biz>

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -30,4 +30,6 @@ Dummy::Application.configure do
 
   # Expands the lines which load the assets
   # config.assets.debug = true
+
+  config.active_storage.service = :local
 end
--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -54,4 +54,6 @@ Dummy::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.active_storage.service = :local
 end
