0479c3dddb0d80faa003f8fa443e6d542a8b898d
Ryota Suzuki <ryota.suzuki@linkers.net>
2021-01-27 18:23:54 +0900

    fix helper_proxy

--- lib/draper/helper_proxy.rb
+++ lib/draper/helper_proxy.rb
@@ -8,9 +8,9 @@ module Draper
     end
 
     # Sends helper methods to the view context.
-    def method_missing(method, *args, &block)
+    def method_missing(method, *args, **kwargs, &block)
       self.class.define_proxy method
-      send(method, *args, &block)
+      send(method, *args, **kwargs, &block)
     end
 
     # Checks if the context responds to an instance method, or is able to
@@ -28,8 +28,8 @@ module Draper
     private
 
     def self.define_proxy(name)
-      define_method name do |*args, &block|
-        view_context.send(name, *args, &block)
+      define_method name do |*args, **kwargs, &block|
+        view_context.send(name, *args, **kwargs, &block)
       end
     end
   end
