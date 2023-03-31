82cc153fb7c5dad7069a100992cd528ca09ff440
Fredrik Palmkron <jonkerz@gmail.com>
2023-03-31 21:40:04 +0200

    Replace deprecated/removed `File.exists?` alias

--- spec/performance/benchmark.rb
+++ spec/performance/benchmark.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 Bundler.require :default
 
 require "benchmark"
