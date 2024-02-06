a1439fff830ce6206ef3599b728ebd8485f6a190
Alexander Senko <Alexander.Senko@gmail.com>
2024-02-07 02:41:58 +0700

    Integration with `Turbo::Broadcastable`
    Overriding defaults for Turbo broadcast jobs allows one to get decorated
    objects in model partials by default.
    
    Resolves drapergem/draper#910.
    Requires drapergem/draper#928.

--- spec/dummy/spec/models/post_spec.rb
+++ spec/dummy/spec/models/post_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Post do
   it_behaves_like 'a decoratable model'
 
   it { should be_a ApplicationRecord }
+
+  describe 'broadcasts' do
+    let(:modification) { described_class.create! }
+
+    it 'passes a decorated object for rendering' do
+      expect do
+        modification
+      end.to have_enqueued_job(Turbo::Streams::ActionBroadcastJob).with { |stream, action:, target:, **rendering|
+        expect(rendering[:locals]).to include :post
+        expect(rendering[:locals][:post]).to be_decorated
+      }
+    end
+  end if defined? Turbo::Broadcastable
 end
